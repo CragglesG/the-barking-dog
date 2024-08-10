f4fe0d9ba842e5fe6e136d07a49b0e689acb3e5f
Craig Giles <121510295+CragglesG@users.noreply.github.com>
2024-08-10 16:18:01 +0100

    Set up RPi environemnt and fixed bugs

--- run.py
+++ run.py
@@ -6,8 +6,8 @@ from playsound import playsound
 
 
 # Set up GPIO pins
-GPIO.setmode(GPIO.BOARD)    
-PIR_PIN = 2
+GPIO.setmode(GPIO.BCM)    
+PIR_PIN = 10
 GPIO.setup(PIR_PIN, GPIO.IN)
 
 cam = picam()
@@ -26,11 +26,11 @@ try:
         cam.start()
         sleep(1)
         cam.switch_mode_and_capture_file(config, "image.jpg")
-        cam.close()
         subprocess.run(["python3", "identify.py"])
         with open("result.txt", "r") as result:
             res = result.read()
             if res == 'y':
+                print('RABBIT DETECTED!!! BARK DOG, BARK!!!')
                 playsound("./dog-barking-70772.mp3")
 
 except KeyboardInterrupt:
